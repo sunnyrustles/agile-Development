e754bd7d1151bb7ff84422acf60b14d07f90ae81
Shelly M <shellymclallen@yahoo.com>
2016-02-05 10:56:54 -0800

    Added Devise Database - Pages not tested yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'page/navigate'
   get 'page/about'
   get 'page/profile'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+include Faker
+
+10.times do
+
+  puts Company.bs
+
+ # Product.create(
+ #     name: Name.name ,
+ #     description: Lorem.paragraphs(10)
+ # )
+
+  User.create(
+
+          email: Faker::Internet.email,
+          password: Faker::Internet.password(8, 20),
+          sign_in_count: Faker::Number.between(1, 10),
+          #created_at: Faker::Date.between(750.days.ago, 350.days.ago),
+          #updated_at: Faker::Date.between(350.days.ago, Date.today)
+          #username: Faker::Internet.user_name
+    )
+
+ # Profile.create( )
+
+end

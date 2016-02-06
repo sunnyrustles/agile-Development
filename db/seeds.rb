f6a2130ca6a19ebb49de00688d686dff175c9b0e
Shelly M <shellymclallen@yahoo.com>
2016-02-05 17:13:40 -0800

    Seeds Working

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,23 +11,30 @@ include Faker
 
   puts Company.bs
 
- # Product.create(
- #     name: Name.name ,
- #     description: Lorem.paragraphs(10)
- # )
+  Product.create(
+     name: Name.name ,
+      description: Lorem.paragraphs(10)
+ )
 
-=begin
   User.create(
 
-          email: Faker::Internet.email,
-          password: Faker::Internet.password(8, 20),
-          sign_in_count: Faker::Number.between(1, 10),
+          email: Faker::Internet.email,                   # required
+          password: Faker::Internet.password(8, 20),      # required
+          sign_in_count: Faker::Number.between(1, 10),    # required
+          reset_password_token: Faker::Internet.password(8, 20),
+          reset_password_sent_at: Faker::Time.between(DateTime.now  - 350, DateTime.now),
+          remember_created_at: Faker::Time.between(DateTime.now  - 750, DateTime.now - 350),
+          current_sign_in_at:  Faker::Time.between(DateTime.now - 1, DateTime.now),
+          last_sign_in_at:  Faker::Time.between(DateTime.now - 1, DateTime.now),
+          last_sign_in_ip: Faker::Internet.ip_v4_address,
+          current_sign_in_ip: Faker::Internet.ip_v4_address
+
           #created_at: Faker::Date.between(750.days.ago, 350.days.ago),
           #updated_at: Faker::Date.between(350.days.ago, Date.today)
-          #username: Faker::Internet.user_name
     )
-=end
 
- # Profile.create( )
+ # Profile.create(
+ #   #username: Faker::Internet.user_name
+ # )
 
 end

995d9e31c8eb992a5842554f6fb0072554266ab0
Shelly M <shellymclallen@yahoo.com>
2016-02-08 22:38:32 -0800

    Adding a quick seeds for profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205073936) do
-
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 20160205062725) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "fullName"

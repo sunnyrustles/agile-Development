2eb5f9efcb7e4d0f8243c1a8478c6e68eededd65
smclallen <shellymclallen@yahoo.com>
2016-02-19 10:54:00 -0800

    Nate's email verification and hopefully my photo stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_one :profile
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
